e13c5d7bc9a713bd2cfd28478e289d378bf4c0d7
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-06-26 10:28:06 +0700

    cat

---
+++ scripts/components/catCard.js
@@ -0,0 +1,29 @@
+class CatCard extends HTMLElement {
+  catName;
+  catImage;
+  catTag;
+  constructor() {
+    super();
+    this.catName = this.getAttribute("data-catName");
+    this.catImage = this.getAttribute("data-catImage");
+    this.catTag = this.getAttribute("data-catTag");
+
+    this.setAttribute('class', 'cat-card');
+
+    this.innerHTML = `
+      <div class="cat__image">
+        <img src="${this.catImage}" alt="${this.catName}">
+      </div>
+      <div class="cat__desc">
+        <div class="cat__name">
+          ${this.catName}
+        </div>
+        <div class="cat__tag">
+          ${this.catTag}
+        </div>
+      </div>
+    `
+  }
+}
+
+customElements.define("cat-card", CatCard);
